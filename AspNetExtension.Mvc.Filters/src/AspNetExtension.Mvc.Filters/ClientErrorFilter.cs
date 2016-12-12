2eda94b3930dbd53c941c9660ce0fd93d99569b8
NicatorBa <daniel@bartschinski.info>
2016-12-12 20:47:04 +0100

    Project library creation

---
+++ AspNetExtension.Mvc.Filters/src/AspNetExtension.Mvc.Filters/ClientErrorFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AspNetExtension.Mvc.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class ClientErrorFilter : Attribute, IAsyncActionFilter, IAsyncExceptionFilter
+    {
+        public Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OnExceptionAsync(ExceptionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AspNetExtension.Mvc.Filters/src/AspNetExtension.Mvc.Filters/ErrorResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetExtension.Mvc.Filters
+{
+    public class ErrorResponse
+    {
+        public string Type { get; set; }
+
+        public string Description { get; set; }
+
+        public string Info { get; set; }
+
+        public object ModelErrors { get; set; }
+    }
+}
